115075bfe437119e6b6e6c6d0d8eb9a437426c02
Luka Mihic <lukamihiic@gmail.com>
2019-07-09 08:36:50 +0200

    added divs which will contain charts and tables in today view

--- src/views/Today.vue
+++ src/views/Today.vue
@@ -5,7 +5,13 @@
     <br>
     <h2>Graphs present your rating results. Today you have {{ today }} rates,</h2>
     <h2>check it on the dashboard.</h2>
+    <br>
+    <div id="lineChart"><p>Place for line chart</p></div>
+    <div id="pieChart"><p>Place for pie chart</p></div>
+    <br>
+    <div id="dataTable"><p>Place for data table</p></div>
   </div>
+  
 </template>
 
 <script>
@@ -19,5 +25,26 @@ data:function(){
 </script>
 
 <style>
-
+#lineChart{
+  background:rgb(36, 40, 46);
+  height:400px;
+  width:55%;
+  float:left;
+  margin-left:150px;
+  margin-right:20px;
+}
+#pieChart{
+  background:rgb(36, 40, 46);
+  width:30%;
+  height:400px;
+  float:left;
+}
+#dataTable{
+  float:left;
+  background:rgb(36, 40, 46);
+  height:400px;
+  width:45%;
+  margin-left:150px;
+  margin-top:25px;
+}
 </style>
