96588d12b5bb87755572703256b922d2e656a0ac
IgorSiljegovic <igor97.mo@gmail.com>
2019-07-11 15:08:03 -0400

    Create get request for thank you messages

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,14 +1,18 @@
-import axios from "axios"
+import axios from 'axios';
 
-const API_URL = "http://172.20.15.96:3000"
+const API_URL = 'http://172.20.15.52:3000';
 
 export default {
-	getActiveSettings() {
-		return axios.get(`${API_URL}/settings`)
-			.then(response => response.data)
-	},
-	postSettings(settings) {
-		return axios.post(`${API_URL}/settings`, settings)
-			.then(response => response.data)
-	},
-}
+  getActiveSettings() {
+    return axios.get(`${API_URL}/settings`)
+      .then(response => response.data);
+  },
+  postSettings(settings) {
+    return axios.post(`${API_URL}/settings`, settings)
+      .then(response => response.data);
+  },
+  getThanksMessages() {
+    return axios.get(`${API_URL}/messages`)
+      .then(response => response.data);
+  }
+};
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,4 @@
 <template>
-<div id="settings">
   <v-app>
     <v-form>
       <v-container class="settings" fluid>
@@ -7,17 +6,7 @@
         <v-divider class="divider" dark/>
         <v-layout>
           <v-flex md6>
-            <p>Emotions preview</p>
-            <!--<v-text-field
-              dark color="grey"
-              v-model="activeSettings.emotions"
-              label="Emotions preview">
-            </v-text-field>-->
-            <img class="emoPreview" src="../assets/happyPreview.png">
-            <img class="emoPreview" v-show="activeSettings.emotionsNum>3" src="../assets/between1Preview.png">
-            <img class="emoPreview" src="../assets/mehPreview.png">
-            <img class="emoPreview" v-show="activeSettings.emotionsNum>4" src="../assets/between2Preview.png">
-            <img class="emoPreview" src="../assets/sadPreview.png">
+            <label>Emotions preview</label>
           </v-flex>
 
           <v-flex md6>
@@ -35,7 +24,7 @@
           <v-flex>
             <v-text-field
               dark color="grey"
-              v-model="activeSettings.emotionsNum"
+              v-model="activeSettings.emoticonNumber"
               :rules="emotionsRules"
               label="Number of emotions"
               type="number"
@@ -59,79 +48,64 @@
       </v-container>
     </v-form>
   </v-app>
-  </div>
 </template>
 
 <script>
-import ApiService from "@/services/ApiService"
+import ApiService from '@/services/ApiService';
 
 export default {
-	data() {
-		return {
-			activeSettings: {
-				emotions: " ",
-				message: "Thanks",
-				emotionsNum: 5,
-				messageTimeout: 7,
-			},
-			messages: ["Thanks", "Thank you", "Thank you for your rating", "Hvala Vam na ocjeni", "Hvala"],
-			emotionsRules: [
-				v => !!v || "Emotions number is required",
-				v => v > 2 || "Min value is 3",
-				v => v < 6 || "Max value is 5",
-			],
-			timeoutRules: [
-				v => !!v || "Message timeout is required",
-				v => v > 0 || "Min value is 1",
-				v => v < 11 || "Max value is 10",
-			],
-		}
-	},
-	methods: {
-		validateSettings() {
-			return this.activeSettings.message.length > 0
+  data() {
+    return {
+      activeSettings: {},
+      messages: [],
+      emotionsRules: [
+        v => v > 2 || 'Min value is 3',
+        v => v < 6 || 'Max value is 5',
+      ],
+      timeoutRules: [
+        v => !!v || 'Message timeout is required',
+        v => v > 0 || 'Min value is 1',
+        v => v < 11 || 'Max values is 10',
+      ],
+    };
+  },
+  methods: {
+    validateSettings() {
+      return this.activeSettings.message.length > 0
         && this.activeSettings.message.length < 121
         && this.activeSettings.emotionsNum > 2
         && this.activeSettings.emotionsNum < 6
         && this.activeSettings.messageTimeout > 0
-        && this.activeSettings.messageTimeout < 11
-		},
-		getActiveSettings() {
-			ApiService.getActiveSettings()
-				.then((response) => {
-					this.activeSettings = response.data
-				})
-		},
-	},
-	created() {
-		this.getActiveSettings()
-	},
-}
+        && this.activeSettings.messageTimeout < 11;
+    },
+    getActiveSettings() {
+      ApiService.getActiveSettings()
+        .then((response) => {
+          this.activeSettings = response.data[0];
+        });
+    },
+    getThanksMessages() {
+      ApiService.getThanksMessages()
+        .then((response) => {
+          this.messages = response.data;
+        });
+    },
+  },
+  created() {
+    this.getActiveSettings();
+    this.getThanksMessages();
+  },
+};
 </script>
 
 <style>
-.settings {
-    width: 80%;
-    background: rgb(18, 20, 22);
+  .settings {
+    width: 90%;
   }
   .divider {
     margin: 20px 0;
   }
-  #settings {
-    float: left;
-    margin-left: 150px;
-    width:calc(100% - 200px);
-    background: rgb(18, 20, 22);
-  }
-  .application--wrap{
-    background: rgb(18, 20, 22);
-  }
-  .emoPreview{
-    height:40px;
-    margin-right:10px;
-    width:auto;
-  }
-  #settings h1 {
-    margin-left: 20px !important;
+  h1 {
+    text-align: left;
   }
 </style>
